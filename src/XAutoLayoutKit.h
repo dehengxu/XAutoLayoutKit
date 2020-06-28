c3627950891443ce0051eaca503cd0052fc353ac
DehengXu <dehengxu@outlook.com>
2020-06-28 09:54:35 +0800

    [Update] compiler params

--- src/XAutoLayoutKit.h
+++ src/XAutoLayoutKit.h
@@ -13,7 +13,15 @@
 #import "XAutoLayoutBinder.h"
 #import "NSLayoutConstraint+XALK.h"
 
-FOUNDATION_EXTERN NSString * const kXAutoLayoutVersion;
+FOUNDATION_EXTERN NSString *const kXAutoLayoutVersion;
+
+#ifndef XALK_WARN_UNUSED_RESULT
+    #define XALK_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#endif
+
+#ifndef XALK_DEPRECATED_MSG
+    #define XALK_DEPRECATED_MSG(x) __deprecated_msg(x)
+#endif
 
 #pragma mark - UIView category
 
@@ -24,50 +32,50 @@ FOUNDATION_EXTERN NSString * const kXAutoLayoutVersion;
 
 #pragma mark - set size
 
-- (instancetype)setSameSizeWithView:(UIView *)view;
-- (instancetype)setSameWidthWithView:(UIView *)view;
-- (instancetype)setSameHeightWithView:(UIView *)view;
-- (instancetype)setConstraintSize:(CGSize)size;
-- (instancetype)setConstraintSize:(CGSize)size onSuperView:(UIView *)superView __deprecated_msg("should use -(instancetype)setConstraintSize: instead after 0.2.0");
-- (instancetype)setConstraintWidth:(CGFloat)width;
-- (instancetype)setConstraintWidth:(CGFloat)width onSuperView:(UIView *)superView __deprecated_msg("should use -(instancetype)setConstraintWidth: instead after 0.2.0");
-- (instancetype)setConstraintHeight:(CGFloat)height;
-- (instancetype)setConstraintHeight:(CGFloat)height onSuperView:(UIView *)superView __deprecated_msg("should use -(instancetype)setConstraintHeight: instead after 0.2.0");
+- (instancetype)setSameSizeWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
+- (instancetype)setSameWidthWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
+- (instancetype)setSameHeightWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
+- (instancetype)setConstraintSize:(CGSize)size XALK_WARN_UNUSED_RESULT;
+- (instancetype)setConstraintSize:(CGSize)size onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintSize: instead after 0.2.0");
+- (instancetype)setConstraintWidth:(CGFloat)width XALK_WARN_UNUSED_RESULT;
+- (instancetype)setConstraintWidth:(CGFloat)width onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintWidth: instead after 0.2.0");
+- (instancetype)setConstraintHeight:(CGFloat)height  XALK_WARN_UNUSED_RESULT;
+- (instancetype)setConstraintHeight:(CGFloat)height onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintHeight: instead after 0.2.0");
 
 #pragma mark - follow position
 
-- (instancetype)followCenterXOfView:(UIView *)v1;
-- (instancetype)followCenterYOfView:(UIView *)v1;
-- (instancetype)followCenterOfView:(UIView *)v1;
+- (instancetype)followCenterXOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followCenterYOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followCenterOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
 
-- (instancetype)followX:(CGFloat)x OfView:(UIView *)v1 __deprecated_msg("should use alignTo... serial methods to instead");
-- (instancetype)followY:(CGFloat)y OfView:(UIView *)v1 __deprecated_msg("should use alignTo... serial methods to instead");
-- (instancetype)followPositionOfView:(UIView *)v1 __deprecated_msg("should use alignToPoint serial methods to instead");
-    
-#pragma mark - follow size
-    
-- (instancetype)followWidthOfView:(UIView *)v1;
-- (instancetype)followWidthOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier;
-- (instancetype)followHeightOfView:(UIView *)v1;
-- (instancetype)followHeightOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier;
-- (instancetype)followSizeOfView:(UIView *)v1;
-- (instancetype)followSizeOfView:(UIView *)v1 withMultipliers:(NSArray *)multipliers;
+- (instancetype)followX:(CGFloat)x OfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
+- (instancetype)followY:(CGFloat)y OfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
+- (instancetype)followPositionOfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignToPoint serial methods to instead");
+
+#pragma mark - following APIs
+
+- (instancetype)followWidthOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followWidthOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier XALK_WARN_UNUSED_RESULT;
+- (instancetype)followHeightOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followHeightOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier XALK_WARN_UNUSED_RESULT;
+- (instancetype)followSizeOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followSizeOfView:(UIView *)v1 withMultipliers:(NSArray *)multipliers XALK_WARN_UNUSED_RESULT;
 
 #pragma mark - align of view
 
-- (instancetype)alignToLeft:(CGFloat)margin ofView:(UIView *)v1;
-- (instancetype)alignToRight:(CGFloat)margin ofView:(UIView *)v1;
-- (instancetype)alignToTop:(CGFloat)margin ofView:(UIView *)v1;
-- (instancetype)alignToBottom:(CGFloat)margin ofView:(UIView *)v1;
-- (instancetype)alignToCenter:(CGPoint)point ofView:(UIView *)v1;
-- (instancetype)alignToPoint:(CGPoint)location ofView:(UIView *)v1;
+- (instancetype)alignToLeft:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToRight:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToTop:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToBottom:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToCenter:(CGPoint)point ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToPoint:(CGPoint)location ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
 
 #pragma mark - out of view
 
-- (instancetype)above:(CGFloat)above ofView:(UIView *)v1;
-- (instancetype)below:(CGFloat)below ofView:(UIView *)v1;
-- (instancetype)ahead:(CGFloat)ahead ofView:(UIView *)v1;
-- (instancetype)after:(CGFloat)after ofView:(UIView *)v1;
+- (instancetype)above:(CGFloat)above ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)below:(CGFloat)below ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)ahead:(CGFloat)ahead ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)after:(CGFloat)after ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
 
 @end
 
@@ -78,5 +86,3 @@ FOUNDATION_EXPORT double XAutoLayoutKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char XAutoLayoutKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XAutoLayoutKit/PublicHeader.h>
-
-
