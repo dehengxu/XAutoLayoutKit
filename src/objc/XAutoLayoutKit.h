bd2f85b8a281b7b7d20b0ab2c63763696a0a9e21
DehengXu <xdh@meitu.com>
2020-10-03 17:10:56 +0800

    Update: remove XALK_WARN_UNUSED_RESULT

--- src/objc/XAutoLayoutKit.h
+++ src/objc/XAutoLayoutKit.h
@@ -13,7 +13,8 @@
 #import "XAutoLayoutBinder.h"
 
 #ifndef XALK_WARN_UNUSED_RESULT
-#define XALK_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#define XALK_WARN_UNUSED_RESULT  
+//__attribute__((warn_unused_result))
 #endif
 
 #ifndef XALK_DEPRECATED_MSG
@@ -26,76 +27,76 @@ FOUNDATION_EXTERN NSString *const kXAutoLayoutVersion;
 
 @interface UIView (LayoutConstraint)
 
-- (instancetype)alk_enableAutoLayout XALK_WARN_UNUSED_RESULT;
-- (instancetype)alk_disableAutoLayout XALK_WARN_UNUSED_RESULT;
+- (instancetype)alk_enableAutoLayout ;
+- (instancetype)alk_disableAutoLayout ;
 
 #pragma mark - set size
 
-- (instancetype)setSameSizeWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
-- (instancetype)setSameWidthWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
-- (instancetype)setSameHeightWithView:(UIView *)view XALK_WARN_UNUSED_RESULT;
-- (instancetype)setConstraintSize:(CGSize)size XALK_WARN_UNUSED_RESULT;
-- (instancetype)setConstraintSize:(CGSize)size onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintSize: instead after 0.2.0");
-- (instancetype)setConstraintWidth:(CGFloat)width XALK_WARN_UNUSED_RESULT;
-- (instancetype)setConstraintWidth:(CGFloat)width onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintWidth: instead after 0.2.0");
-- (instancetype)setConstraintHeight:(CGFloat)height  XALK_WARN_UNUSED_RESULT;
-- (instancetype)setConstraintHeight:(CGFloat)height onSuperView:(UIView *) superView XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintHeight: instead after 0.2.0");
+- (instancetype)setSameSizeWithView:(UIView *)view ;
+- (instancetype)setSameWidthWithView:(UIView *)view ;
+- (instancetype)setSameHeightWithView:(UIView *)view ;
+- (instancetype)setConstraintSize:(CGSize)size ;
+- (instancetype)setConstraintSize:(CGSize)size onSuperView:(UIView *) superView  XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintSize: instead after 0.2.0");
+- (instancetype)setConstraintWidth:(CGFloat)width ;
+- (instancetype)setConstraintWidth:(CGFloat)width onSuperView:(UIView *) superView  XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintWidth: instead after 0.2.0");
+- (instancetype)setConstraintHeight:(CGFloat)height  ;
+- (instancetype)setConstraintHeight:(CGFloat)height onSuperView:(UIView *) superView  XALK_DEPRECATED_MSG("should use -(instancetype)setConstraintHeight: instead after 0.2.0");
 
 #pragma mark - follow position
 
-- (instancetype)followCenterXOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)followCenterYOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)followCenterOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followCenterXOfView:(UIView *)v1 ;
+- (instancetype)followCenterYOfView:(UIView *)v1 ;
+- (instancetype)followCenterOfView:(UIView *)v1 ;
 
-- (instancetype)followX:(CGFloat)x OfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
-- (instancetype)followY:(CGFloat)y OfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
-- (instancetype)followPositionOfView:(UIView *) v1 XALK_WARN_UNUSED_RESULT XALK_DEPRECATED_MSG("should use alignToPoint serial methods to instead");
+- (instancetype)followX:(CGFloat)x OfView:(UIView *) v1  XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
+- (instancetype)followY:(CGFloat)y OfView:(UIView *) v1  XALK_DEPRECATED_MSG("should use alignTo... serial methods to instead");
+- (instancetype)followPositionOfView:(UIView *) v1  XALK_DEPRECATED_MSG("should use alignToPoint serial methods to instead");
 
 #pragma mark - following APIs
 
-- (instancetype)followWidthOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)followWidthOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier XALK_WARN_UNUSED_RESULT;
-- (instancetype)followHeightOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)followHeightOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier XALK_WARN_UNUSED_RESULT;
-- (instancetype)followSizeOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)followSizeOfView:(UIView *)v1 withMultipliers:(NSArray *)multipliers XALK_WARN_UNUSED_RESULT;
-- (instancetype)followSafeAreaOfView:(UIView*)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)followWidthOfView:(UIView *)v1 ;
+- (instancetype)followWidthOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier ;
+- (instancetype)followHeightOfView:(UIView *)v1 ;
+- (instancetype)followHeightOfView:(UIView *)v1 withMultiplier:(CGFloat)multiplier ;
+- (instancetype)followSizeOfView:(UIView *)v1 ;
+- (instancetype)followSizeOfView:(UIView *)v1 withMultipliers:(NSArray *)multipliers ;
+- (instancetype)followSafeAreaOfView:(UIView*)v1 ;
 
 #pragma mark - align of view
 
-- (instancetype)alignToLeft:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToRight:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToTop:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToBottom:(CGFloat)margin ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToCenter:(CGPoint)point ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToPoint:(CGPoint)location ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToLeft:(CGFloat)margin ofView:(UIView *)v1 ;
+- (instancetype)alignToRight:(CGFloat)margin ofView:(UIView *)v1 ;
+- (instancetype)alignToTop:(CGFloat)margin ofView:(UIView *)v1 ;
+- (instancetype)alignToBottom:(CGFloat)margin ofView:(UIView *)v1 ;
+- (instancetype)alignToCenter:(CGPoint)point ofView:(UIView *)v1 ;
+- (instancetype)alignToPoint:(CGPoint)location ofView:(UIView *)v1 ;
 
 #pragma mark - out of view
 
-- (instancetype)above:(CGFloat)above ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)below:(CGFloat)below ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)ahead:(CGFloat)ahead ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)after:(CGFloat)after ofView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)above:(CGFloat)above ofView:(UIView *)v1 ;
+- (instancetype)below:(CGFloat)below ofView:(UIView *)v1 ;
+- (instancetype)ahead:(CGFloat)ahead ofView:(UIView *)v1 ;
+- (instancetype)after:(CGFloat)after ofView:(UIView *)v1 ;
 
 #pragma mark - relate to anchor
 
-- (instancetype)alignToLeft:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToRight:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToTop:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToBottom:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToCenter:(CGPoint)point anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)alignToPoint:(CGPoint)location anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
+- (instancetype)alignToLeft:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)alignToRight:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)alignToTop:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)alignToBottom:(CGFloat)margin anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)alignToCenter:(CGPoint)point anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)alignToPoint:(CGPoint)location anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
 
 #if 0
-- (instancetype)above:(CGFloat)above anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)below:(CGFloat)below anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)ahead:(CGFloat)ahead anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
-- (instancetype)after:(CGFloat)after anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea XALK_WARN_UNUSED_RESULT;
+- (instancetype)above:(CGFloat)above anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)below:(CGFloat)below anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)ahead:(CGFloat)ahead anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
+- (instancetype)after:(CGFloat)after anchorOfView:(UIView *)v1 withSafeArea:(BOOL)isSafeArea ;
 
-- (instancetype)above:(CGFloat)above anchorOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)below:(CGFloat)below anchorOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)ahead:(CGFloat)ahead anchorOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
-- (instancetype)after:(CGFloat)after anchorOfView:(UIView *)v1 XALK_WARN_UNUSED_RESULT;
+- (instancetype)above:(CGFloat)above anchorOfView:(UIView *)v1 ;
+- (instancetype)below:(CGFloat)below anchorOfView:(UIView *)v1 ;
+- (instancetype)ahead:(CGFloat)ahead anchorOfView:(UIView *)v1 ;
+- (instancetype)after:(CGFloat)after anchorOfView:(UIView *)v1 ;
 #endif
 
 @end
